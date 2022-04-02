0b3ac6d83559610565c8bcd7aa9ac27a8565a960
therakib7 <therakib7@gmail.com>
2022-04-02 11:05:51 +0600

    added current class

--- src/admin/js/react/dashboard.jsx
+++ src/admin/js/react/dashboard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';  
+import ReactDOM from 'react-dom';
 import Home from 'components/home';
 import AppContext from 'context/app-context';
-import msgData from 'context/data/msg'; 
+import msgData from 'context/data/msg';
 
-function Dashboard() { 
+function Dashboard() {
     return (
         <>
             <AppContext.Provider value={{
@@ -18,17 +18,31 @@ function Dashboard() {
     )
 }
 
-document.addEventListener( 'DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     //TODO: collapse menu for main page
     // document.body.className+=' folded';  
-    let element = document.getElementById( 'ncpi-dashboard' );
-    if( typeof element !== 'undefined' && element !== null ) {
-        ReactDOM.render( 
+    let element = document.getElementById('ncpi-dashboard');
+    if (typeof element !== 'undefined' && element !== null) {
+        ReactDOM.render(
             <React.StrictMode>
                 <Dashboard />
-            </React.StrictMode>, 
-            element 
+            </React.StrictMode>,
+            element
         );
     }
-} );
 
+    //show current menu in dashbaord 
+    let navUl = document.querySelectorAll('#toplevel_page_ncpi ul > li');
+    for (let y = 0, l = navUl.length; y < l; y++) {
+        //TODO: set current class when reload
+        //let link = navUl[y].getElementsByTagName('a');
+        navUl[y].addEventListener('click', checkLi);
+    }
+    function checkLi() {
+        for (let y = 0, l = navUl.length; y < l; y++) {
+            navUl[y].classList.remove('current');
+        }
+        this.classList.add('current');
+    }
+
+});
