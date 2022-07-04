3394c9a6d2ad13340d16134d4b48df821e007a76
therakib7 <therakib7@gmail.com>
2022-07-04 20:05:15 +0600

    added theme class name

--- app/Ctrls/Filter/Types/BodyClass.php
+++ app/Ctrls/Filter/Types/BodyClass.php
@@ -1,34 +1,40 @@
 <?php
-namespace Ncpi\Ctrls\Filter\Types; 
 
-class BodyClass {
+namespace Ncpi\Ctrls\Filter\Types;
 
-    public function __construct() {   
-        add_filter( 'body_class', [$this, 'body_class'] );
-		add_filter( 'admin_body_class', [$this, 'admin_body_class'] );
-    }   
+class BodyClass
+{
 
-    function body_class( $classes ) {
-		if (  
-			is_page_template( [
-				'dashboard-template.php', 
+	public function __construct()
+	{
+		add_filter('body_class', [$this, 'body_class']);
+		add_filter('admin_body_class', [$this, 'admin_body_class']);
+	}
+
+	function body_class($classes)
+	{
+		if (
+			is_page_template([
+				'dashboard-template.php',
 				'invoice-template.php',
 				'estimate-template.php'
-			] )  
-		) { 
+			])
+		) {
 			$classes[] = 'ncpi';
+			$classes[] = get_option('template') . '-theme';
 		}
 		return $classes;
 	}
 
-	function admin_body_class( $classes ) { 
-		if ( 
-			( isset( $_GET['page'] ) && $_GET['page'] == 'ncpi' ) ||
-			( isset( $_GET['page'] ) && $_GET['page'] == 'ncpi-welcome' ) 
-		) { 
-			$classes .= ' ncpi';
+	function admin_body_class($classes)
+	{
+		if (
+			(isset($_GET['page']) && $_GET['page'] == 'ncpi') ||
+			(isset($_GET['page']) && $_GET['page'] == 'ncpi-welcome')
+		) {
+			$classes .= ' ncpi ' . get_option('template') . '-theme';
 		}
-	 
+
 		return $classes;
-	} 
+	}
 }
