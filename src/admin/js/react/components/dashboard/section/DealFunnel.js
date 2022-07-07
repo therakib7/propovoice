77dcc2c4cb70fc68106d6955030ce16d1857aadc
Rakib <therakib7@gmail.com>
2022-07-07 11:42:03 +0600

    working with dashboard

--- src/admin/js/react/components/dashboard/section/DealFunnel.js
+++ src/admin/js/react/components/dashboard/section/DealFunnel.js
@@ -23,7 +23,25 @@ const Summary = (props) => {
             >
                 Deal Funnel
             </h3>
-            <ul> 
+            <ul>
+                <li style={{ background: "#d6defa" }}>
+                    Interested<span>12</span>
+                </li>
+                <li style={{ background: "#acbef4", width: "80%" }}>
+                    In Conversastion<span>12</span>
+                </li>
+                <li style={{ background: "#829dee", width: "60%" }}>
+                    Qualified<span>12</span>
+                </li>
+                <li style={{ background: "#5b7cea", width: "40%" }}>
+                    Offer Sent<span>12</span>
+                </li>
+                <li style={{ background: "#345bde" }}>
+                    <span>12</span> Deal Won
+                </li>
+            </ul>
+            <p className="pi-text-center">Deal Lost 8</p>
+            {/* <ul> 
                 {funnel.common.map((item, i) => (
                     <li key={i} style={{ color: item.color, backgroundColor: item.bg_color, width: item.width }}>
                         {item.name}<span>{item.items}</span>
@@ -33,9 +51,9 @@ const Summary = (props) => {
                 {funnel.won && <li style={{ color: funnel.won.color, backgroundColor: funnel.won.bg_color }}>
                     <span>{funnel.won.items}</span> Deal Won
                 </li>}
-            </ul>
+            </ul> 
+            {funnel.lost && <p className="pi-text-center">Deal Lost {funnel.lost.items}</p>} */}
 
-            {funnel.lost && <p className="pi-text-center">Deal Lost {funnel.lost.items}</p>}
         </div>
     );
 }
