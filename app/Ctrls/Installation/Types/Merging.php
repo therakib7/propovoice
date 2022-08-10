5f58b6c44fcd0fb860987e924f41510b6b80e383
Rakib <therakib7@gmail.com>
2022-08-10 12:15:08 +0600

    working with merging

---
+++ app/Ctrls/Installation/Types/Merging.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ncpi\Ctrls\Installation\Types; 
+
+class Merging {
+
+    public function __construct() {  
+        $this->extra_amount(); 
+    }   
+
+    function extra_amount() { 
+        
+    } 
+ 
+}
