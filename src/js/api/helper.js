2a1ae82460682cd85fe4bc1d523849443bc0e914
Azhar Ibn Mostafiz <theazharul@gmail.com>
2022-09-12 16:41:12 +0600

    api helper function created

--- src/js/api/helper.js
+++ src/js/api/helper.js
@@ -1,10 +1,64 @@
-export const apiUrl = `${ndpv.apiUrl}ndpv/v1/`;
-export const apiProUrl = `${ndpv.apiUrl}ndpvp/v1/`;
-export const apiServerUrl = `${ndpv.apiServerUrl}ncpis/v1/`;
+import axios from "axios";
+const apiUrl = `${ndpv.apiUrl}ndpv/v1/`;
+const apiProUrl = `${ndpv.apiUrl}ndpvp/v1/`;
+const apiServerUrl = `${ndpv.apiServerUrl}ncpis/v1/`;
+const url = apiUrl + "invoices";
+const path = "invoices";
+const urlServer = apiServerUrl + path;
 
-export const token = {
-	headers: {
-		'content-type': 'application/json',
-		'X-WP-NONCE': ndpv.nonce
-	}
+const token = {
+  headers: {
+    "content-type": "application/json",
+    "X-WP-NONCE": ndpv.nonce,
+  },
 };
+
+const makeRequest = ({
+  method = `get`,
+  urlParts = {},
+  formData = {},
+  headers = token,
+  requestFrom = `free`,
+}) => {
+  const baseUrl = getBaseUrl(requestFrom);
+  const requestToUrl = urlBuilder(baseUrl, urlParts);
+  return axios({
+    method: method,
+    url: requestToUrl,
+    data: formData,
+    headers,
+  });
+};
+
+const urlBuilder = (baseUrl, urlParts = {}) => {
+  const fullUrl = Object.entries(urlParts).reduce((url, [key, part]) => {
+    let newUrl;
+    switch (key) {
+      case "path":
+        newUrl = url + `${part}/`;
+        break;
+      case "params":
+        newUrl = url + `?${part}`;
+        break;
+      case "fragment":
+        newUrl = url + `#${part}`;
+        break;
+    }
+
+    return newUrl;
+  }, baseUrl);
+  return fullUrl;
+};
+
+const getBaseUrl = (requestFrom) => {
+  const urls = {
+    free: `${ndpv.apiUrl}ndpv/v1/`,
+    pro: `${ndpv.apiUrl}ndpvp/v1/`,
+    external: `${ndpv.apiServerUrl}ncpis/v1/`,
+  };
+  return urls[requestFrom];
+};
+
+export default { makeRequest };
+
+export { apiUrl, apiProUrl, apiServerUrl, token };
