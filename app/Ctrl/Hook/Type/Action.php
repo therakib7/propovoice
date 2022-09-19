871a8c7de9c06672544e314f60d2aee27f88c26d
Azhar Ibn Mostafiz <theazharul@gmail.com>
2022-09-19 11:21:27 +0600

    paid status in invoice and redirect to invoice

--- app/Ctrl/Hook/Type/Action.php
+++ app/Ctrl/Hook/Type/Action.php
@@ -2,20 +2,35 @@
 
 namespace Ndpv\Ctrl\Hook\Type;
 
+use Ndpv\Helper\Fns;
+
 class Action
 {
-	public function __construct()
-	{
+    public function __construct()
+    {
+        add_action("woocommerce_order_status_completed", [
+            $this,
+            "order_complete",
+        ]);
+    }
 
-		add_action('woocommerce_payment_complete', [$this, 'ndpv_order_complete']);
-	}
+    function order_complete($order_id)
+    {
+        $invoice_id = get_post_meta($order_id, "_ndpv_invoice_id", true);
 
-
-	function ndpv_order_complete($order_id)
-	{
-		$invoice_id = get_post_meta($order_id, "_ndpv_invoice_id", true);
-		if ($invoice_id) {
-			update_post_meta($invoice_id, 'status', 'paid');
-		}
-	}
+        if ($invoice_id) {
+            update_post_meta($invoice_id, "status", "paid");
+        }
+        if (!is_admin()) {
+            wp_safe_redirect(
+                sprintf(
+                    "%s?id=%s&token=%s",
+                    Fns::client_page_url("invoice"),
+                    $invoice_id,
+                    get_post_meta($invoice_id, "token", true)
+                )
+            );
+            exit();
+        }
+    }
 }
