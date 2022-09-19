05d4258e00c040783863bc4116809a976c403d71
Azhar Ibn Mostafiz <theazharul@gmail.com>
2022-09-19 11:39:34 +0600

    wc action hook moved to pro

--- app/Ctrl/Hook/Type/Action.php
+++ app/Ctrl/Hook/Type/Action.php
@@ -2,35 +2,9 @@
 
 namespace Ndpv\Ctrl\Hook\Type;
 
-use Ndpv\Helper\Fns;
-
 class Action
 {
     public function __construct()
     {
-        add_action("woocommerce_order_status_completed", [
-            $this,
-            "order_complete",
-        ]);
-    }
-
-    function order_complete($order_id)
-    {
-        $invoice_id = get_post_meta($order_id, "_ndpv_invoice_id", true);
-
-        if ($invoice_id) {
-            update_post_meta($invoice_id, "status", "paid");
-        }
-        if (!is_admin()) {
-            wp_safe_redirect(
-                sprintf(
-                    "%s?id=%s&token=%s",
-                    Fns::client_page_url("invoice"),
-                    $invoice_id,
-                    get_post_meta($invoice_id, "token", true)
-                )
-            );
-            exit();
-        }
     }
 }
