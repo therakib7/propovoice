c431071dd5c62b08c60f291209940ac7d9e6bad3
therakib7 <therakib7@gmail.com>
2022-09-20 21:14:54 +0600

    update

--- src/js/cpnt/setting/tab/estinv/common/index.js
+++ src/js/cpnt/setting/tab/estinv/common/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 import Currency from './sub/Currency';
 import QtyType from './sub/QtyType';
 import ExtraAmount from './sub/ExtraAmount';
@@ -9,28 +9,28 @@ export default class Main extends Component {
 		const i18n = ndpv.i18n;
 		this.state = {
 			tabs: [
-				/* {
+				{
 					id: 'currency',
 					text: i18n.cur
-				}, */
+				},
 				{
 					id: 'qty-type',
-					text: i18n.qty + ' ' +i18n.type
+					text: i18n.qty + ' ' + i18n.type
 				},
 				{
 					id: 'extra-amount',
-					text: i18n.adtl + ' ' +i18n.amt
+					text: i18n.adtl + ' ' + i18n.amt
 				},
 			],
 			currentTab: '',
-		} 
-	} 
-
-	componentDidMount() {
-		this.setState({ currentTab: 'qty-type' });
+		}
 	}
 
-	setActiveTab(id) { 
+	componentDidMount() {
+		this.setState({ currentTab: 'currency' });
+	}
+
+	setActiveTab(id) {
 		this.setState({ currentTab: id });
 	}
 
@@ -49,10 +49,10 @@ export default class Main extends Component {
 						</li>
 					))}
 				</ul>
-		  
-				{currentTab == 'currency' && <Currency {...this.props} />} 
-				{currentTab == 'qty-type' && <QtyType {...this.props} />} 
-				{currentTab == 'extra-amount' && <ExtraAmount {...this.props} />} 
+
+				{currentTab == 'currency' && <Currency {...this.props} />}
+				{currentTab == 'qty-type' && <QtyType {...this.props} />}
+				{currentTab == 'extra-amount' && <ExtraAmount {...this.props} />}
 			</>
 		);
 	}
