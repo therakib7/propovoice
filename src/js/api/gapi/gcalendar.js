be2ebe91da57ee29d5228986862c9cf67d8d43fb
Azhar Ibn Mostafiz <theazharul@gmail.com>
2022-10-06 17:43:44 +0600

    creating calendar event

--- src/js/api/gapi/gcalendar.js
+++ src/js/api/gapi/gcalendar.js
@@ -1,31 +1,54 @@
- event = {
-   // summary: "Google I/O 2015",
-   // location: "800 Howard St., San Francisco, CA 94103",
-   // description: "A chance to hear more about Google's developer products.",
-   // start: {
-   //   dateTime: "2015-05-28T09:00:00-07:00",
-   //   timeZone: "America/Los_Angeles",
-   // },
-   // end: {
-   //   dateTime: "2015-05-28T17:00:00-07:00",
-   //   timeZone: "America/Los_Angeles",
-   // },
-   // recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
-   attendees: [{ email: "lpage@example.com" }, { email: "sbrin@example.com" }],
-   // reminders: {
-   //   useDefault: false,
-   //   overrides: [
-   //     { method: "email", minutes: 24 * 60 },
-   //     { method: "popup", minutes: 10 },
-   //   ],
-   // },
- };
+import { handleSignIn } from "api/gapi/goauth2";
 
- request = gapi.client.calendar.events.insert({
-   calendarId: "primary",
-   resource: self.event,
- });
+export const createEvent = () => {
+  handleSignIn(() => {
+    myEvent();
+  });
+};
 
- request.execute(function (event) {
-   appendPre("Event created: " + event.htmlLink);
- });
+const myEvent = async () => {
+  const meetingRequestId = crypto.randomUUID();
+  event = {
+    summary: "Google I/O 2015",
+    // location: "800 Howard St., San Francisco, CA 94103",
+    description: "A chance to hear more about Google's developer products.",
+    start: {
+      dateTime: "2022-10-07T09:00:00-07:00",
+      timeZone: "America/Los_Angeles",
+    },
+    end: {
+      dateTime: "2022-10-07T17:00:00-07:00",
+      timeZone: "America/Los_Angeles",
+    },
+    // recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
+    attendees: [
+      { email: "theazharul@gmail.com" },
+      { email: "sbrin@example.com" },
+    ],
+    conferenceData: {
+      createRequest: {
+        requestId: meetingRequestId,
+        conferenceSolutionKey: {
+          type: "hangoutsMeet",
+        },
+      },
+    },
+    // reminders: {
+    //   useDefault: false,
+    //   overrides: [
+    //     { method: "email", minutes: 24 * 60 },
+    //     { method: "popup", minutes: 10 },
+    //   ],
+    // },
+  };
+
+  const request = await gapi.client.calendar.events.insert({
+    calendarId: "primary",
+    resource: event,
+  });
+  console.log(request);
+  // request.execute(function (event) {
+  //   console.log(event);
+  //   appendPre("Event created: " + event.htmlLink);
+  // });
+};
