21a643b9930588f263f795d38cae3585a09083e4
Azhar Ibn Mostafiz <theazharul@gmail.com>
2022-10-06 14:59:31 +0600

    api_key added in integration settings

--- src/js/api/gapi/goauth2.js
+++ src/js/api/gapi/goauth2.js
@@ -2,29 +2,30 @@ import api from "api";
 
 let tokenClient;
 
-const loadScript = (file) => {
+const loadScript = (id, file, callAfterLoad) => {
   const script = document.createElement("script");
   script.async = true;
   script.defer = true;
   script.src = file;
+  script.id = id;
+  script.onload = callAfterLoad;
   document.body.appendChild(script);
-
-  script.addEventListener("load", () => {
-    if (typeof gapi !== "undefined") gapiLoaded();
-    if (typeof google !== "undefined") gisLoaded();
-  });
 };
 
-loadScript("https://apis.google.com/js/api.js");
-loadScript("https://accounts.google.com/gsi/client");
+loadScript("gapi", "https://apis.google.com/js/api.js", gapiLoaded);
+loadScript("gsi", "https://accounts.google.com/gsi/client", gisLoaded);
 
-const getCredentials = () => {
+(() => {
   return api.get("settings", "tab=google_api_oauth2").then((resp) => {
     if (resp.data.success) {
-      return resp.data.data;
+      localStorage.setItem("g_client_id", resp.data.data.client_id);
+      localStorage.setItem("g_api_key", resp.data.data.api_key);
     }
   });
-};
+})();
+
+const CLIENT_ID = localStorage.getItem("g_client_id");
+const API_KEY = localStorage.getItem("g_api_key");
 
 const DISCOVERY_DOC =
   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
@@ -37,17 +38,17 @@ function gapiLoaded() {
 }
 
 async function initializeGapiClient() {
-  const credentials = await getCredentials();
+  console.log(API_KEY);
   await gapi.client.init({
-    apiKey: credentials.api_key,
+    apiKey: API_KEY,
     discoveryDocs: [DISCOVERY_DOC],
   });
 }
 
 async function gisLoaded() {
-  const credentials = await getCredentials();
+  console.log(CLIENT_ID);
   tokenClient = google.accounts.oauth2.initTokenClient({
-    client_id: credentials.client_id,
+    client_id: CLIENT_ID,
     scope: SCOPES,
     callback: "", // defined later
   });
