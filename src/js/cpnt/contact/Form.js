7c6c27744025161d8cec176a4e0b404582ffdb21
Rakib <therakib7@gmail.com>
2022-10-24 10:23:50 +0600

    fixed some translation issues

--- src/js/cpnt/contact/Form.js
+++ src/js/cpnt/contact/Form.js
@@ -80,7 +80,7 @@ export default class Form extends Component {
 
     handleContactSelect = (val, type) => {
         let form = { ...this.state.form }
-         
+
         if (type == 'person') {
             form.first_name = val.first_name;
             form.person_id = (val) ? val.id : null;
@@ -137,7 +137,7 @@ export default class Form extends Component {
     render() {
         const form = this.state.form;
         const i18n = ndpv.i18n;
-        const modalType = this.props.modalType == 'new' ? i18n.new : i18n.edit;
+        const modalType = this.props.modalType == 'new' ? i18n.add + ' ' + i18n.new : i18n.edit;
         return (
             <div className="pv-overlay pv-show">
                 <div className="pv-modal-content">
@@ -146,7 +146,7 @@ export default class Form extends Component {
                         <span className="pv-close" onClick={() => this.props.close()}>
                             <Add />
                         </span>
-                        <h2 className="pv-modal-title">{modalType} {i18n.ct}</h2> 
+                        <h2 className="pv-modal-title">{modalType} {i18n.ct}</h2>
                         <p>{sprintf(i18n.formDesc, modalType, i18n.ct)}</p>
                     </div>
 
