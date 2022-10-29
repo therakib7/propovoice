7a48da7da96e717fdb9af14c7ad2c436e879f792
Bahauddin paloan <82359680+bahauddinmnb20@users.noreply.github.com>
2022-10-29 15:04:24 +0600

    import - downlode file add

--- src/js/block/add-new/ImportModal.js
+++ src/js/block/add-new/ImportModal.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import WithRouter from 'hoc/Router';
+import { toast } from 'react-toastify';
 import { Add } from 'block/icon';
+import { CSVLink } from "react-csv";
+import csv from './csv';
 
 const ImportModal = (props) => {
     const [file, setFile] = useState();
@@ -18,6 +21,7 @@ const ImportModal = (props) => {
         setArray1(value)
 
     }
+
     const csvFileToArray = string => {
         const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
         const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
@@ -63,7 +67,10 @@ const ImportModal = (props) => {
             };
 
             fileReader.readAsText(file);
+        } else {
+            toast('Please upload file')
         }
+
     };
     const data = Object.values(Object.assign({}, ...array));
     const headerKeys = Object.keys(Object.assign({}, ...array));
@@ -83,39 +90,40 @@ const ImportModal = (props) => {
                 <form  >
                     <div className="pv-content">
                         <div className="pv-form-style-one">
+                            {!file && <div className="up">
+                                <div className="pv-upload-file">
+                                    <input
+                                        type={"file"}
+                                        id={"files"}
+                                        accept={".csv"}
+                                        onChange={handleOnChange}
+                                        className="d-none"
+                                    />
+                                    <label htmlFor="files" className=' pv-btn pv-bg-stroke pv-bg-hover-stroke  '>
+                                        <svg
+                                            width={25}
+                                            height={25}
+                                            viewBox="0 0 14 14"
+                                            fill="none"
 
-                            <div className="pv-upload-file">
-                                <input
-                                    type={"file"}
-                                    id={"csvFileInput"}
-                                    accept={".csv"}
-                                    onChange={handleOnChange}
-                                    className="d-none"
-                                />
-                                <label htmlFor="csvFileInput" className=' pv-btn pv-bg-stroke pv-bg-hover-stroke  '>
-                                    <svg
-                                        width={25}
-                                        height={25}
-                                        viewBox="0 0 14 14"
-                                        fill="none"
-
-                                    >
-                                        <path
-                                            d="M3.66824 10.0907C3.01193 10.0915 2.37842 9.85536 1.88909 9.42768C1.39976 9 1.08901 8.41081 1.01638 7.773C0.943746 7.13518 1.11434 6.49358 1.49547 5.97112C1.87661 5.44866 2.44148 5.08208 3.08188 4.94161C2.89659 4.09662 3.06217 3.21426 3.5422 2.48865C4.02223 1.76304 4.77738 1.25361 5.64153 1.07243C6.50568 0.891248 7.40804 1.05316 8.1501 1.52254C8.89217 1.99193 9.41315 2.73034 9.59844 3.57533H9.66507C10.4913 3.57451 11.2883 3.87392 11.9014 4.41541C12.5146 4.9569 12.9001 5.70185 12.9831 6.50564C13.0662 7.30943 12.8408 8.11472 12.3508 8.76517C11.8608 9.41562 11.1411 9.86483 10.3314 10.0256M8.99875 8.13612L6.99981 6.1815M6.99981 6.1815L5.00087 8.13612M6.99981 6.1815V13"
-                                            stroke="#4C6FFF"
-                                            strokeWidth="1.5"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                        />
-                                    </svg>
-                                    Click to upload File
-                                </label>
-                            </div>
-                            <div className="pv-import">
-                                <div className="pv-sample-file">
-                                    <a style={{ color: 'rgb(54 91 243)', fontWeight: '500' }} href="#">Download sample file</a>
-                                    {!file && <p>Please make sure your csv file has unique headers.Otherwise, it may fail to import</p>}
+                                        >
+                                            <path
+                                                d="M3.66824 10.0907C3.01193 10.0915 2.37842 9.85536 1.88909 9.42768C1.39976 9 1.08901 8.41081 1.01638 7.773C0.943746 7.13518 1.11434 6.49358 1.49547 5.97112C1.87661 5.44866 2.44148 5.08208 3.08188 4.94161C2.89659 4.09662 3.06217 3.21426 3.5422 2.48865C4.02223 1.76304 4.77738 1.25361 5.64153 1.07243C6.50568 0.891248 7.40804 1.05316 8.1501 1.52254C8.89217 1.99193 9.41315 2.73034 9.59844 3.57533H9.66507C10.4913 3.57451 11.2883 3.87392 11.9014 4.41541C12.5146 4.9569 12.9001 5.70185 12.9831 6.50564C13.0662 7.30943 12.8408 8.11472 12.3508 8.76517C11.8608 9.41562 11.1411 9.86483 10.3314 10.0256M8.99875 8.13612L6.99981 6.1815M6.99981 6.1815L5.00087 8.13612M6.99981 6.1815V13"
+                                                stroke="#4C6FFF"
+                                                strokeWidth="1.5"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                        Click to upload File
+                                    </label>
                                 </div>
+                            </div>}
+                            <div className="pv-import">
+                                {!file && <div className="pv-sample-file">
+                                    <CSVLink style={{ color: 'rgb(54 91 243)', fontWeight: '500' }} data={csv}>Download sample file</CSVLink>
+                                    {!file && <p>Please make sure your csv file has unique headers.Otherwise, it may fail to import</p>}
+                                </div>}
                                 <div className="pv-import-map">
                                     <button
                                         className="pv-btn pv-btn-medium pv-bg-blue pv-bg-hover-blue pv-bg-shadow pv-color-white"
@@ -123,11 +131,11 @@ const ImportModal = (props) => {
                                             handleOnSubmit(e);
                                         }}
                                     >
-                                        {/* {props.title} {i18n.imp} */}
                                         Next [Map Columns]
                                     </button>
                                 </div>
                             </div>
+
                             <br />
                             {file && <div className='pv-table-wrap pv-mb-20'>
                                 <table className='pv-table'>
