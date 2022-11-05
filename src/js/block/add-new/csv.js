1206f0f2824b2a4c1a73d5b390e7c79325327e69
Bahauddin paloan <82359680+bahauddinmnb20@users.noreply.github.com>
2022-11-05 11:37:42 +0600

    update import

--- src/js/block/add-new/csv.js
+++ src/js/block/add-new/csv.js
@@ -1,5 +1,5 @@
 export default [
-    ["name", "organization", "email","phone",	"country",	"currency",	"tag",	"level"],
+    ["Person", "Organization", "Email","Mobile",	"country",	"currency",	"tag",	"level"],
     ["Ahmed", "nurency", "ah@smthing.co.com"," 0125745624",	"bangladesh",	"USD($)",	"design",	"hot"],
     ["Raed", "Labes", "rl@smthing.co.com"],
     ["Yezzi", "Min l3b", "ymin@cocococo.com"]
