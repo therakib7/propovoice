d52ca9441185c416da70818c9469b696028259f1
Bahauddin paloan <82359680+bahauddinmnb20@users.noreply.github.com>
2022-11-08 17:57:36 +0600

    file name changed

---
+++ src/js/block/add-new/EntityFields.js
@@ -0,0 +1,26 @@
+export default {
+
+    lead: {
+        not_asign: 'Not Asign',
+        first_name: 'Person',
+        org_name: 'Organization',
+        email: 'Email',
+        mobile: 'Mobile',
+        budget: 'Budget',
+        currency: 'Currency',
+        desc: 'Description',
+        note: 'Note',
+    },
+    client: {
+        not_asign: 'Not Asign',
+        first_name: 'Person',
+        org_name: 'Organization',
+        email: 'Email',
+        mobile: 'Mobile',
+        web: 'Website',
+        country: 'Country',
+        region: 'Region',
+        address: 'Address',
+    }
+
+}
