a71523fa8112abc9858fed9d8ac0f073525c8a67
Rakib <therakib7@gmail.com>
2022-12-29 13:41:41 +0600

    working with update

--- app/Ctrl/Install/Type/Update.php
+++ app/Ctrl/Install/Type/Update.php
@@ -15,15 +15,21 @@ class Update
     public function info()
     {
         $option = get_option("ndpv_subscription");
-        $subscribe = false;
-        $share = true;
+        $get_update = $share_info = true;
+        $get_update_at = $share_info_at = false;
         if ( $option ) {
-            $data = $option;
+            $get_update = ( isset( $option['get_update'] ) && !$option['get_update'] ) ? false : true;
+            $share_info = ( isset( $option['share_info'] ) && !$option['share_info'] ) ? false : true;
+
+            $get_update_at = isset( $option['get_update_at'] ) ? true : false;
+            $share_info_at = isset( $option['share_info_at'] ) ? true : false;
         }
 
-        if ( !$subscribe ) {
-            $info = new Info;
-            $data = $info->wp( $share );
+        if ( ( $get_update || $share_info ) && ( !$get_update_at || !$share_info_at ) ) {
+
+            // return;
+            /* $info = new Info;
+            $data = $info->wp( $share_info_at );
             $data['version'] = NDPV_VERSION;
             $data['package'] = 'free';
 
@@ -32,9 +38,31 @@ class Update
                 'body' => $data,
                 'blocking' => false,
                 'sslverify' => false
-            ] );
+            ] ); */
+            $data = null;
+            if ( $option ) {
+                if ( !$get_update_at ) {
+                    $option['get_update_at'] = current_time('timestamp');
+                }
 
-            // update_option('ndpv_version_install', NDPV_VERSION);
+                if ( !$share_info_at ) {
+                    $option['share_info_at'] = current_time('timestamp');
+                }
+
+                $data = $option;
+            } else {
+                if ( !$get_update_at ) {
+                    $data['get_update_at'] = current_time('timestamp');
+                }
+
+                if ( !$share_info_at ) {
+                    $data['share_info_at'] = current_time('timestamp');
+                }
+            }
+
+            if ( $data ) {
+                update_option('ndpv_subscription', $data);
+            }
         }
 
         /* $version = get_option('ndpv_version_install');
