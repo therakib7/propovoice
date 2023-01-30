03d8dfa9973ad751497384546723aea96842d87d
Azhar Ibn Mostafiz <theazharul@gmail.com>
2023-01-30 09:29:28 +0600

    caps checking in menu

--- app/Ctrl/Setting/Type/Dashboard.php
+++ app/Ctrl/Setting/Type/Dashboard.php
@@ -1,113 +1,129 @@
-<?php 
+<?php
 namespace Ndpv\Ctrl\Setting\Type;
 
 use Ndpv\Helper\Fns;
 
 class Dashboard
 {
-
     public function __construct()
     {
-        add_action('admin_menu', [$this, 'add_settings_menu'], 30);
+        add_action("admin_menu", [$this, "add_settings_menu"], 30);
     }
 
     public function add_settings_menu()
     {
         add_menu_page(
-            esc_html__('Propovoice', 'propovoice'),
-            esc_html__('Propovoice', 'propovoice'),
-            'manage_options',
-            'ndpv',
-            array($this, 'main_settings'),
-            'dashicons-groups',
+            esc_html__("Propovoice", "propovoice"),
+            esc_html__("Propovoice", "propovoice"),
+            "manage_options",
+            "ndpv",
+            [$this, "main_settings"],
+            "dashicons-groups",
             2
         );
 
         add_submenu_page(
-            'ndpv',
-            esc_html__('Dashboard', 'propovoice'),
-            esc_html__('Dashboard', 'propovoice'),
-            'manage_options',
-            'ndpv#',
-            array($this, 'render')
+            "ndpv",
+            esc_html__("Dashboard", "propovoice"),
+            esc_html__("Dashboard", "propovoice"),
+            "ndpv_dashboard",
+            "ndpv#",
+            [$this, "render"]
         );
 
         $settings_menu = [
             [
-                'id' => 'lead',
-                'label' => esc_html__('Lead', 'propovoice'),
+                "id" => "lead",
+                "label" => esc_html__("Lead", "propovoice"),
+                "capability" => "ndpv_lead",
             ],
             [
-                'id' => 'deal',
-                'label' => (ndpv()->wage()) ? esc_html__('Deal Pipeline', 'propovoice') : esc_html__('Deal', 'propovoice'),
+                "id" => "deal",
+                "label" => ndpv()->wage()
+                    ? esc_html__("Deal Pipeline", "propovoice")
+                    : esc_html__("Deal", "propovoice"),
+                "capability" => "ndpv_deal",
             ],
             [
-                'id' => 'estimate',
-                'label' => esc_html__('Estimate', 'propovoice'),
+                "id" => "estimate",
+                "label" => esc_html__("Estimate", "propovoice"),
+                "capability" => "ndpv_estimate",
             ],
             /* [
                 'id' => 'quotation',
                 'label' => esc_html__('Quotation', 'propovoice'),
             ], */
             [
-                'id' => 'invoice',
-                'label' => esc_html__('Invoice', 'propovoice'),
+                "id" => "invoice",
+                "label" => esc_html__("Invoice", "propovoice"),
+                "capability" => "ndpv_invoice",
             ],
             [
-                'id' => 'client',
-                'label' => esc_html__('Client', 'propovoice'),
+                "id" => "client",
+                "label" => esc_html__("Client", "propovoice"),
+                "capability" => "ndpv_client",
             ],
             [
-                'id' => 'project',
-                'label' => esc_html__('Project', 'propovoice'),
+                "id" => "project",
+                "label" => esc_html__("Project", "propovoice"),
+                "capability" => "ndpv_project",
             ],
             [
-                'id' => 'contact',
-                'label' => esc_html__('Contact', 'propovoice'),
-            ]
-        ]; 
-        
-        $settings_menu = apply_filters('ndpv_sidebar_menu', $settings_menu);
+                "id" => "contact",
+                "label" => esc_html__("Contact", "propovoice"),
+                "capability" => "ndpv_contact",
+            ],
+        ];
+
+        $settings_menu = apply_filters("ndpv_sidebar_menu", $settings_menu);
 
         foreach ($settings_menu as $menu) {
-            $menu_id = $menu['id'];
-            if ($menu_id == 'contact') {
-                $menu_id = $menu_id . '/person';
+            $menu_id = $menu["id"];
+            if ($menu_id == "contact") {
+                $menu_id = $menu_id . "/person";
             }
 
             add_submenu_page(
-                'ndpv',
-                $menu['label'],
-                $menu['label'],
-                'manage_options',
-                'ndpv#/' . $menu_id,
-                array($this, 'render')
+                "ndpv",
+                $menu["label"],
+                $menu["label"],
+                $menu["capability"],
+                "ndpv#/" . $menu_id,
+                [$this, "render"]
             );
         }
 
         add_submenu_page(
-            'ndpv',
-            esc_html__('Settings', 'propovoice'),
-            esc_html__('Settings', 'propovoice'),
-            'manage_options',
-            'ndpv#/setting/general',
-            array($this, 'render')
+            "ndpv",
+            esc_html__("Settings", "propovoice"),
+            esc_html__("Settings", "propovoice"),
+            "manage_options",
+            "ndpv#/setting/general",
+            [$this, "render"]
         );
 
         if (ndpv()->wage()) {
             global $submenu;
-            $permalink = Fns::client_page_url('workspace');
+            $permalink = Fns::client_page_url("workspace");
             if ($permalink) {
-                $submenu['ndpv'][] = array(esc_html__('Go to Frontend', 'propovoice'), 'manage_options', $permalink);
+                $submenu["ndpv"][] = [
+                    esc_html__("Go to Frontend", "propovoice"),
+                    "manage_options",
+                    $permalink,
+                ];
             }
         }
 
-        if (!function_exists('ndpvp')) {
+        if (!function_exists("ndpvp")) {
             global $submenu;
-            $submenu['ndpv'][] = array('Upgrade to Pro', 'manage_options', 'https://propovoice.com');
+            $submenu["ndpv"][] = [
+                "Upgrade to Pro",
+                "manage_options",
+                "https://propovoice.com",
+            ];
         }
 
-        remove_submenu_page('ndpv', 'ndpv');
+        remove_submenu_page("ndpv", "ndpv");
     }
 
     function main_settings()
