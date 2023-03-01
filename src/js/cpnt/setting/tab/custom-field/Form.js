b9ecf659fc6410c8e4706a6c7e51a23f90a488e6
therakib7 <therakib7@gmail.com>
2023-03-01 11:17:50 +0600

    update form

--- src/js/cpnt/setting/tab/custom-field/Form.js
+++ src/js/cpnt/setting/tab/custom-field/Form.js
@@ -55,9 +55,6 @@ export default class Form extends Component {
         newForm.mod = this.props.mod;
 
         if (this.props.modalType == 'new') {
-            if (this.props.extra_amount_type) {
-                newForm.extra_amount_type = this.props.extra_amount_type;
-            }
             api.add('custom-fields', newForm).then(resp => {
                 if (resp.data.success) {
                     toast.success(ndpv.i18n.aAdd);
