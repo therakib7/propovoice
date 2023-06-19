807894f8285081334f8ce5812ac0c890b70588c1
Azhar Ibn Mostafiz <theazharul@gmail.com>
2023-06-19 10:50:40 +0600

    dropdown style changed

--- src/js/block/dropdown/NotificationDropdown.js
+++ src/js/block/dropdown/NotificationDropdown.js
@@ -34,39 +34,77 @@ const NotificationDropdown = (props) => {
     });
   };
 
+  const dropdownWraperStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "16px",
+    width: "260px",
+    height: "482px",
+    zIndex: "9",
+    position: 'absolute',
+    top: "55px",
+    background: "#FFFFFF",
+    border: "1px solid #E2E8F0",
+    boxShadow: "0px 4.95074px 6.60099px rgba(32, 29, 67, 0.07)",
+    borderRadius: "10px",
+    padding: "16px"
+  }
+  const listStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "14px",
+    overflow: "auto",
+    marginRight: "4px"
+  }
+
+  const listItmeStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "4px",
+  }
   const blueCircleStyle = {
     height: "6px",
     width: "6px",
+    marginLeft: "6px",
     backgroundColor: "#4C6FFF",
     borderRadius: "50%",
     display: "inline-block"
+  }
+
+  const howLongStyle = {
+    fontFamily: 'Inter',
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: "10px",
+    lineHeight: "100%",
+    color: "#4C6FFF",
   }
 
 
 
   return (
     <>
-      <div className="pv-dropdown-content pv-show" >
-        <h3 style={{ padding: '12px 16px' }}>Notifications</h3>
+      <div className="" style={dropdownWraperStyle}>
+        <h3 style={{}}>Notifications</h3>
 
-        <div style={{ display: 'inline-flex', gap: '8px', padding: '12px 16px' }}>
+        <div style={{ display: 'inline-flex', gap: '8px' }}>
           <button className='pv-btn pv-btn-small pv-bg-stroke pv-bg-hover-shadow pv-active' onClick={() => { handleFilterOnClick("all") }}>All</button>
           <button className='pv-btn pv-btn-small pv-bg-stroke pv-bg-hover-shadow' onClick={() => { handleFilterOnClick("unseen") }}>Unread</button>
           <button className='pv-btn pv-btn-small pv-bg-stroke pv-bg-hover-shadow' onClick={() => { handleMarkAllAsReadOnClick() }}>Mark all as read</button>
         </div>
-        < ul >
 
+        < ul style={listStyle}>
           {userNotifications && userNotifications.map((item, index) => {
             const isSeen = parseInt(item.is_seen);
             return (
-              <li style={{}} key={index} onClick={() => { handleNotificationOnClick(item.notification_id) }}>
-                <div style={{}}>
-                  <div style={{}} dangerouslySetInnerHTML={{ __html: item.message }}></div>
+              <li style={listItmeStyle} key={index} onClick={() => { handleNotificationOnClick(item.notification_id) }}>
+                <div style={{ display: "flex", gap: "4px", alignItems: "center", }}>
+                  <div style={{ flexGrow: "1" }} dangerouslySetInnerHTML={{ __html: item.message }}></div>
                   {!isSeen &&
                     (< div style={blueCircleStyle}></div>)
                   }
                 </div>
-                <div >
+                <div style={howLongStyle}>
                   {moment.utc(item.created_at).local().startOf('seconds').fromNow()}
                 </div>
               </li>
