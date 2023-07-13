97b86cdf1a0c343b794610ae5e14ab5ba399abd2
therakib7 <therakib7@gmail.com>
2023-07-13 10:40:03 +0600

    update version to 1.6.3

--- propovoice.php
+++ propovoice.php
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
  * Plugin URI: https://wordpress.org/plugins/propovoice
  * Author: Propovoice
  * Author URI: https://propovoice.com
- * Version: 1.6.2
+ * Version: 1.6.3
  * Description: Lead, Deal, Estimate, Invoice, Billing, Client, Project Automation
  * Text Domain: propovoice
  * Domain Path: /languages
