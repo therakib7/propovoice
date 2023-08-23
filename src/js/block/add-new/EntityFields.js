8492535ed3cf8e0e71c77a161fa13b1fa8cdc98e
Azhar Ibn Mostafiz <theazharul@gmail.com>
2023-08-23 13:07:35 +0600

    fixed export

--- src/js/block/add-new/EntityFields.js
+++ src/js/block/add-new/EntityFields.js
@@ -1,26 +1,26 @@
 export default {
 
-    lead: {
-        not_assign: 'Not Assign',
-        first_name: 'Person',
-        org_name: 'Organization',
-        email: 'Email',
-        mobile: 'Mobile',
-        budget: 'Budget',
-        currency: 'Currency',
-        desc: 'Description',
-        note: 'Note',
-    },
-    client: {
-        not_assign: 'Not Assign',
-        first_name: 'Person',
-        org_name: 'Organization',
-        email: 'Email',
-        mobile: 'Mobile',
-        web: 'Website',
-        country: 'Country',
-        region: 'Region',
-        address: 'Address',
-    }
+        lead: {
+                not_assign: 'Not Assign',
+                first_name: 'Person',
+                org_name: 'Organization',
+                email: 'Email',
+                mobile: 'Mobile',
+                budget: 'Budget',
+                currency: 'Currency',
+                desc: 'Description',
+                // note: 'Note',
+        },
+        client: {
+                not_assign: 'Not Assign',
+                first_name: 'Person',
+                org_name: 'Organization',
+                email: 'Email',
+                mobile: 'Mobile',
+                web: 'Website',
+                country: 'Country',
+                region: 'Region',
+                address: 'Address',
+        }
 
 }
