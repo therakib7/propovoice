1c0633788298e51772f8e484205abe61c8774507
Prappo Prince <prappo.prince@gmail.com>
2023-10-17 18:00:29 +0600

    few update

--- src/js/cpnt/list-single/index.jsx
+++ src/js/cpnt/list-single/index.jsx
@@ -1176,7 +1176,7 @@ class ListSingle extends Component {
                     <Deal module_id={data.id} data={data} parent={path} />
                   )}
                   {currentTab == "email" && data.id && (
-                    <Email />
+                    <Email  module_id={data.id} data={data} parent={path} />
                   )}
                 </Suspense>
               </div>
